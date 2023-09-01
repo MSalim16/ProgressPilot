2fb028c2c252fbb02cd39e89712398ef8113bb16
MSalim16 <mohamedsalim192002@gmail.com>
2023-09-01 11:10:30 +0100

    fixed routing issue

---
+++ src/pages/LogOnPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const logOnPage = () => {
+  return (
+    <div className='bg-red'>
+      <h1>
+        LogonPage
+      </h1>
+    </div>
+  )
+}
+
+export default logOnPage
